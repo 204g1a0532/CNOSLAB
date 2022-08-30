43bb1c54fc9f66863ea3384e2b193d5573019ac0
204g1a0532 <94513570+204g1a0532@users.noreply.github.com>
2022-08-30 12:12:49 +0530

    OSLAB.c
    Girivardhan.V  Created OSLAB.c

---
+++ OSLAB.c
@@ -0,0 +1,5 @@
+Name Of the Candidate: Girivardhan.V
+   Roll No: 204G1A0532
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
