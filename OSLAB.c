d559322aedb48907d12ca235ae47fd0853a1b7e8
204g1a0532 <94513570+204g1a0532@users.noreply.github.com>
2022-08-30 12:27:45 +0530

    Giri completed this course successfully

--- OSLAB.c
+++ OSLAB.c
@@ -1,8 +1,8 @@
-Name Of the Candidate: Girivardhan.V
+/*Name Of the Candidate: Girivardhan.V
    Roll No: 204G1A0532
    Title of the Experiment: CPU Scheduling Using FCFS Algorithm
    Date of Creation: 30-08-2022
-   Date of Execution: 30-08-2022
+   Date of Execution: 30-08-2022*/
 #include <stdio.h>
     int main(){
 	printf("Hello World\n");
