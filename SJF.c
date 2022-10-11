9959f56a514bda68594f5142403d62941b6e9858
204G1A0532 <204G1A0532@srit.ac.in>
2022-10-11 13:03:25 +0530

    First program completed & output generated

--- SJF.c
+++ SJF.c
@@ -4,12 +4,12 @@
    Date of Creation: 20-09-2022
    Date of Execution: 20-09-2022*/
 #include<stdio.h>
-#include<conio.h>
-main()
+
+void main()
 {
-int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
-tatavg;
-clrscr();
+int p[20], bt[20], wt[20], tat[20], i, k, n, temp; 
+float wtavg,tatavg;
+
 printf("\nEnter the number of processes -- ");
 
 scanf("%d", &n);
@@ -42,5 +42,5 @@ printf("\n\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
-printf("\nAverage Turnaround Time -- %f", tatavg/n); getch();
+printf("\nAverage Turnaround Time -- %f", tatavg/n);
 }
