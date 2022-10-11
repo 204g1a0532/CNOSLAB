ddb622024fa0764365f9bf11f074cbd3bf139222
204g1a0532 <94513570+204g1a0532@users.noreply.github.com>
2022-10-11 10:59:49 +0530

    FCFS.c
    V.Girivardhan created FCFS.c

---
+++ fcfs1.c
@@ -0,0 +1,5 @@
+Name Of the Candidate: V.Girivardhan
+   Roll No: 204G1A0532
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 20-09-2022
+   Date of Execution: 20-09-2022
