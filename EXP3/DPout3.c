3843afe8e640bc127c74706709aa5d6e0d704735
204G1A0532 <204G1A0532@srit.ac.in>
2022-11-29 12:50:50 +0530

    Successfully executed the Exp 3 program

---
+++ EXP3/DPout3.c
@@ -0,0 +1,16 @@
+
+DINING PHILOSOPHER PROBLEM
+Enter the total no. of philosophers: 1 2
+How many are hungry : Enterphilosopher1position:1 2
+Enterphilosopher2position:1.One can eat at a time	2.Two can eat at a time3.Exit
+Enter your choice:1
+
+Allow one philosopher to eat at any time
+
+P 0 is granted to eat
+P 0 is waiting
+P 0 is waiting
+P 0 is granted to eat
+P 0 is waiting1.One can eat at a time	2.Two can eat at a time	3.Exit
+Enter your choice:3
+
