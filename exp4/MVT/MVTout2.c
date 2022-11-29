e8e738f8cd1e8b9f5558e61a25dc45f46af99f78
204G1A0532 <204G1A0532@srit.ac.in>
2022-11-29 12:59:53 +0530

    successfully executed exp4a

---
+++ exp4/MVT/MVTout2.c
@@ -0,0 +1,12 @@
+Enter the total memory available (in Bytes)-- 3
+
+Enter memory required for process 1 (in Bytes) -- 4
+
+Memory is Full
+
+Total Memory Available -- 3
+
+	PROCESS		 MEMORY ALLOCATED 
+
+Total Memory Allocated is 0
+Total External Fragmentation is 3
---
+++ exp4/MVT/MVTout3.c
@@ -0,0 +1,12 @@
+Enter the total memory available (in Bytes)-- 2
+
+Enter memory required for process 1 (in Bytes) -- 3
+
+Memory is Full
+
+Total Memory Available -- 2
+
+	PROCESS		 MEMORY ALLOCATED 
+
+Total Memory Allocated is 0
+Total External Fragmentation is 2
