a9a6fe9cc65e44048ab23482500788c909f8bbd2
204G1A0532 <204G1A0532@srit.ac.in>
2022-12-01 15:34:41 +0530

    My c program on MVT has been executed successflly

---
+++ MVT/out3MVT.c
@@ -0,0 +1,11 @@
+Enter the total memory available (in Bytes)-- 1 2
+
+Enter memory required for process 1 (in Bytes) -- 
+Memory is Full
+
+Total Memory Available -- 1
+
+	PROCESS		 MEMORY ALLOCATED 
+
+Total Memory Allocated is 0
+Total External Fragmentation is 1
