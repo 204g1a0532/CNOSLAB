38f578838c55bd29d65515d32cdaa0c465fa6fd7
204G1A0532 <204G1A0532@srit.ac.in>
2022-12-01 15:39:16 +0530

    My c program on MFT has been executed successflly

---
+++ MFT/out1MFT.c
@@ -0,0 +1,11 @@
+Enter the total memory available (in Bytes) -- 1 2
+Enter the block size (in Bytes) -- 
+Enter the number of processes -- 1 2
+Enter memory required for process 1 (in Bytes)-- 
+No. of Blocks available in memory--0
+
+PROCESS	MEMORYREQUIRED	ALLOCATED	INTERNALFRAGMENTATION
+Memory is Full, Remaining Processes cannot be accomodated
+
+Total Internal Fragmentation is 0
+Total External Fragmentation is 1
---
+++ MFT/out3MFT.c
@@ -0,0 +1,18 @@
+<pre>nter the total memory available (in Bytes) -- 1
+Enter the block size (in Bytes) -- 2
+
+Enter the number of processes -- 2
+Enter memory required for process 1 (in Bytes)-- 
+22
+Enter memory required for process 2 (in Bytes)-- 
+
+22
+
+No. of Blocks available in memory--0
+
+PROCESS	MEMORYREQUIRED	ALLOCATED	INTERNALFRAGMENTATION
+Memory is Full, Remaining Processes cannot be accomodated
+
+Total Internal Fragmentation is 0
+Total External Fragmentation is 1<font color="#26A269"><b>srit@srit-HP-Pro-3090-MT</b></font>:<font color="#12488B"><b>~/204g1a0532/CNOSLAB/MFT</b></font>$ 2
+</pre>
